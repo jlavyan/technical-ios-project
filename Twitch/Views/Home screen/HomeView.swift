81f586f3a929d12c1a63bd7facd5ed8d7c0a1deb
Grigori Jlavyan <jlavyan@gmail.com>
2020-05-25 09:08:24 +0400

    FIX indicator in home screen

--- Twitch/Views/Home screen/HomeView.swift
+++ Twitch/Views/Home screen/HomeView.swift
@@ -24,22 +24,34 @@ struct VideoListView: View {
     /// Model for load video list
     @ObservedObject var viewModel = VideoListModel()
     
+    init() {
+        /// Remove list seperator
+        UITableView.appearance().separatorStyle = .none
+    }
+    
     var body: some View {
-        List {
-            ForEach(self.viewModel.videos, id: \.title){ video in
-                VStack {
-                    NavigationLink(
-                        destination: DetailView(video)
-                    ) {
-                        VideoRow(video: video)
-                            .onAppear {
+        ZStack {
+            List {
+                ForEach(self.viewModel.videos, id: \.title){ video in
+                    VStack {
+                        NavigationLink(
+                            destination: DetailView(video)
+                        ) {
+                            VideoRow(video: video)
+                                .onAppear {
+                            }
                         }
-                    }}
+                    }
+                }
+            }.onAppear {
+                /// Load videos on screen appear
+                self.viewModel.load()
             }
-        }.onAppear {
-            /// Load videos on screen appear
-            self.viewModel.load()
+            
+            ActivityIndicator()
+                .frame(width: 50, height: 50).opacity(self.viewModel.videos.count == 0 ? 1.0 : 0.0)
         }
+
     }
 }
 
